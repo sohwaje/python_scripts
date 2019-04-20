1c9699de066c6952fb99d535fac9947166c73f0d
sohwaje <kissenyo@gmail.com>
2019-04-20 22:12:39 +0900

    이 코드는 지금까지 배운 것(출력, 탈출문자열, 함수, 문자열 포맷 연습 등을)복습하는 코드이다.

---
+++ double_loop.py
@@ -0,0 +1,8 @@
+for i in range(5000):
+    for j in range(3000):
+        #if j > 1000:  #j가 1001이 되면 1001을 출력하고 break
+            print(j)
+            break
+    else:
+        continue
+    break
