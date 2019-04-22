68b1a7ed9f5689977ed025b363bd4d9da322c28d
sohwaje <kissenyo@gmail.com>
2019-04-22 23:13:27 +0900

    dd

---
+++ test.py
@@ -0,0 +1,11 @@
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+jelly_beans, jars, crates = secret_formula(start_point)
+
+print("With a starting point of: {}".format(start_point))
