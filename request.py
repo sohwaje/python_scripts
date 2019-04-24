e50253331bcb88866e706f73630c91a2f29b4f9a
sohwaje <kissenyo@gmail.com>
2019-04-24 23:15:20 +0900

    ㅣㅣ

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-URL = 'http://www.daum.net'
-resp = requests.get(URL)
-if resp.status_code == requests.codes.ok:
-    html = resp.text
-    print(html)
--- url_match.py
+++ url_match.py
@@ -1,1 +1,13 @@
+#www.daum.net에서 http, https 링크를 추출하여 가장 긴 url 길이 순서로 정렬하고, 그 중에서 상위 10개의 url의 응답시간을 출력
+import requests
+URL = 'http://www.daum.net'
+resp = requests.get(URL)
+if resp.status_code == requests.codes.ok:
+    html = resp.text
+
+
+
 import re
+target = re.compile('http.*://[^ ]+.')
+result = target.findall(html)
+print(result)
