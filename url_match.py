8a8585efbee570ad899d49cec79f0bc5ab0c34b0
sohwaje <kissenyo@gmail.com>
2019-04-25 22:31:50 +0900

    jj

--- url_match.py
+++ url_match.py
@@ -1,21 +1,14 @@
 import requests
 import re
-#URL = 'http://www.daum.net'
-#resp = requests.get(URL)
-#if resp.status_code == requests.codes.ok:
-#    html = resp.text
-#target = re.compile('http.*://[^"]+')
-#result = target.findall(html)
-#for k in result:
-#    print(k.replace("'",""))
 
-
+#url의 응답코드가 200ok이면, url을 html이라는 변수에 text로 저장하는 함수
 def get(url):
     resp = requests.get(url)
     if resp.status_code == requests.codes.ok:
         html = resp.text
     return html
 
+#패턴 함수
 def p(pattern):
     result = re.compile(pattern)
     return result
@@ -25,11 +18,16 @@ def p(pattern):
 URL = input("input URL> ")
 html = get(URL)
 
-#찾을 패턴
+#변수 = 패턴
 pattern = 'http.*://[^"]+'
 text = p(pattern)
 
+#html에서 패턴과 일치하는 항목을 찾아 result에 저장.
 result = text.findall(html)
+
+#url 길이 순서로 정렬하여 저장
 sort = sorted(result, key=len)
+
+#url에 붙어 있는 "를 제거
 for k in sort:
     print(k.replace("'",""))
