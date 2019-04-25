baaff77c0530ef9f5012e593326e513bdd844824
sohwaje <kissenyo@gmail.com>
2019-04-25 21:33:32 +0900

    ㅣfff

--- url_match.py
+++ url_match.py
@@ -1,36 +1,35 @@
 import requests
 import re
-URL = 'http://www.daum.net'
-resp = requests.get(URL)
-if resp.status_code == requests.codes.ok:
-    html = resp.text
-target = re.compile('http.*://[^""]+')
-result = target.findall(html)
-for k in result:
-    print(k.replace("'",""))
-
-
-#def get(url):
-#    resp = requests.get(url)
-#    if resp.status_code == requests.codes.ok:
-#        html = resp.text
-#    return html
-
-#def select(string):
-#    target = re.compile(string)
-#    return target
-
-#테스트 해야 함.
-#def url(sort):
-#    sorted_string = sorted(sort)
-#    sorted_string.reverse()
-#    return ''.join(sorted_string)
-
-#URL = input("input URL> ")
-#html = get(URL)
-#string = 'http.*://[^"]+'
-#target = select(string)
+#URL = 'http://www.daum.net'
+#resp = requests.get(URL)
+#if resp.status_code == requests.codes.ok:
+#    html = resp.text
 #target = re.compile('http.*://[^"]+')
 #result = target.findall(html)
 #for k in result:
 #    print(k.replace("'",""))
+
+
+def get(url):
+    resp = requests.get(url)
+    if resp.status_code == requests.codes.ok:
+        html = resp.text
+    return html
+
+def p(pattern):
+    result = re.compile(pattern)
+    return result
+
+
+#URL 입력을 받음
+URL = input("input URL> ")
+html = get(URL)
+
+#찾을 패턴
+pattern = 'http.*://[^"]+'
+text = p(pattern)
+
+result = text.findall(html)
+sort = sorted(result, key=len)
+for k in sort:
+    print(k.replace("'",""))
