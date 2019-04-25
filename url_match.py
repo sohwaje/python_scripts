8f425f68f449229ef83020151e7538ba95fcb68f
sohwaje <kissenyo@gmail.com>
2019-04-25 17:48:12 +0900

    ddd

--- url_match.py
+++ url_match.py
@@ -1,13 +1,36 @@
-#www.daum.net에서 http, https 링크를 추출하여 가장 긴 url 길이 순서로 정렬하고, 그 중에서 상위 10개의 url의 응답시간을 출력
 import requests
+import re
 URL = 'http://www.daum.net'
 resp = requests.get(URL)
 if resp.status_code == requests.codes.ok:
     html = resp.text
-
-
-
-import re
-target = re.compile('http.*://[^ ]+.')
+target = re.compile('http.*://[^""]+')
 result = target.findall(html)
-print(result)
+for k in result:
+    print(k.replace("'",""))
+
+
+#def get(url):
+#    resp = requests.get(url)
+#    if resp.status_code == requests.codes.ok:
+#        html = resp.text
+#    return html
+
+#def select(string):
+#    target = re.compile(string)
+#    return target
+
+#테스트 해야 함.
+#def url(sort):
+#    sorted_string = sorted(sort)
+#    sorted_string.reverse()
+#    return ''.join(sorted_string)
+
+#URL = input("input URL> ")
+#html = get(URL)
+#string = 'http.*://[^"]+'
+#target = select(string)
+#target = re.compile('http.*://[^"]+')
+#result = target.findall(html)
+#for k in result:
+#    print(k.replace("'",""))
