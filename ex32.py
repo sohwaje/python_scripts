6dc6dc02543d68a9fb30c5674144bcb1ec05bafd
sohwaje <kissenyo@gmail.com>
2019-04-27 21:52:20 +0900

    ㅇㅇ

--- ex32.py
+++ ex32.py
@@ -1,37 +1,42 @@
-숫자들 = [1, 2, 3, 4, 5]
-과일들 = ['사과', '귤', '배', '살구']
-잔돈들 = [1, '십원', 2, '백원', 3, '오백원']
+#숫자들 = [1, 2, 3, 4, 5]
+#과일들 = ['사과', '귤', '배', '살구']
+#잔돈들 = [1, '십원', 2, '백원', 3, '오백원']
 
 #첫 번째 for 순환문은 list를 따라 돕니다.
-for 숫자 in 숫자들:
-    print(f"이 수는 {숫자}")
+#for 숫자 in 숫자들:
+#    print(f"이 수는 {숫자}")
 
-for 과일 in 과일들:
-    print(f"과일 종류: {과일}")
+#for 과일 in 과일들:
+#    print(f"과일 종류: {과일}")
 
-for i in 잔돈들:
-    print(f"받은 잔돈 {i}")
+#for i in 잔돈들:
+#    print(f"받은 잔돈 {i}")
 
 #리스트 만들기
 원소들 = []
 
-for i in range(0, 6): # 0, 6 = 5개의 연속된 숫자를 의미한다.
-    print(f"list에 {i} 숫자를 더합니다.")
+# for 함수와 range함수의 조합
+#for i in range(0, 6): # 0, 6 = 5개의 연속된 숫자를 의미한다.
+#    print(f"list에 {i} 숫자를 더합니다.")
     # append는 list가 인식하는 함수이다.
-    원소들.append(i)
+#    원소들.append(i)
 
-for i in 원소들:
-    print(f"원소는 {i}")
+print("range 함수로만 위 로직을 구현하기")
+i = list(range(0, 6))
+print(i)
+
+#for i in 원소들:
+#    print(f"원소는 {i}")
 
 
 #다른 리스트 만들기
-name = []
+#name = []
 
-for i in range(0, 10):
-    name.append(i)
+#for i in range(0, 10):
+#    name.append(i)
 
-for i in name:
-    print(i)
+#for i in name:
+#    print(i)
 
 #quiz
 #[1] 30행에 for문을 쓰지 않고 리스트에 넣어보자.
--- url_match_2.py
+++ url_match_2.py
@@ -9,27 +9,30 @@ def get(url):
     return html
 
 # 패턴 함수
-def p(pattern):
-    result = re.compile(pattern)
+def pattern(p):
+    result = re.compile(p)
     return result
-
 
 # URL 입력을 받음
 URL = 'http://www.naver.com'
+#URL = input('URL> ')
 html = get(URL)
 
-# 변수 = 정규식 표현으로 패턴을 만듦(이 코드가 핵심!!)
-pattern = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+#[s]? = ?앞에 있는 문자 s가 있거나 없거나 모두 매치된다.
+# | = "or"의 의미와 동일하다. A|B는 A또는 B이다. "|"을 findall 함수와 같이 쓰면 A와B를 모두 찾는다.
+# +, * = 반복을 나타내는 메타문자. *은 0번 반복해도 참, +는 최소 1번 반복해야 참.
+# . = 모든 문자와 매치된다.
+# ^ 또는 $ 문자를 메타문자가 아닌 문자 그 자체로 매치하고 싶은 경우에는 [^], [$] 처럼 사용하거나 \^, \$ 로 사용
+p = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
-text = p(pattern)
-
-# html에서 패턴과 일치하는 항목을 찾아 result에 저장.
+text = pattern(p)
+# html에서 패턴과 일치하는 항목을 찾아 list 형식으로 result에 저장.
 result = text.findall(html)
 
 # url의 길이값(length) 순서로 정렬하여 저장
 sort = sorted(result, key=len)
 
-#url에 붙어 있는 "를 제거
+#sort 리스트에서 url을 출력한다.
 for url in sort:
     print(url)
     #print(k.replace("'","")) #정규식 표현에서 "와'를 제거하였으므로 필요 없음.
