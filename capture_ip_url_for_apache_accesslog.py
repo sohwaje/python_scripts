342f5e81c281becee2ebe9c6a30d2081398c0aae
sohwaje <kissenyo@gmail.com>
2019-05-04 13:38:54 +0900

    ff

--- capture_ip_url_for_apache_accesslog.py
+++ capture_ip_url_for_apache_accesslog.py
@@ -3,12 +3,7 @@
 ]# python3.7 accesslog.py 아파치엑세스로그파일명
 """
 """
-[1]문자열("([\d]+[.]+\d+[.]+\d+[.]+[\d])")은 IP와 일치를 시도하는 정규식 표현이다.
-[2]정규식 엔진은 그 다음 한 번 이상 반복되는 모든 문자열(.+)에 대해 최대 일치를 시도하는 시점에서 비캡쳐 문자열(?:)을 만난다.
-[3]그리고 다시 그룹핑된 (.*)에 의해 모든 문자열에 대해서 최대 일치+를 시도하고, 다시 비캡쳐 문자열을 만난다.
-*** 비캡쳐 문자열을 만나면 정규식 엔진은 해당 문자열을 캡쳐는 하지만, 출력은 하지 않는다.
-**마지막 print(result[0]+result[1])은 출력되는 리스트 자료형[ 'x', 'x1']에서 0번째와 1번째의 문자열을 더해서 출력한다.
-이렇게 하는 이유는 리스트 자료형의 구조에서 싱글쿼터와 쉼표를 제외하고 IP와 URL만 출력할 수 있기 때문이다.
+정규식 문자 패턴 분석 : https://regex101.com/r/lIHC9G/1
 """
 from sys import argv
 import re
@@ -17,12 +12,13 @@ script, file_name = argv                        # 스크립트를 실행 할 때
 file = open(file_name, 'r')
 lines = file.readlines()
 
-#access_log에서 IP와 URL 경로 추출
-m = re.compile('([\d]+[.]+\d+[.]+\d+[.]+[\d]).+(?:\"GET|\"POST)(.*?)(?:HTTP)')
+#access_log에서 IP와 http[s]를 포함한 URL 경로 추출
+m = re.compile('(?:[\d]+[.]\d+[.]\d+[.][\d]+)|(?:\s\/.*?\s)|http[s]?://(?:[a-zA-Z0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+num = 0
 for line in lines:
     ip_path = m.findall(line)
-    for result in ip_path:
-        print(result[0]+result[1])   #['IP', 'URL']에서 0번째가 IP, 1번째가 URL이 된다.
+    num = num + 1
+    print(f"{num}",ip_path)
 file.close()
 
 # 정규식 엔진에서 IP만 별도로 추출하거나, GET이나 POST 방식으로 요청된 URL을 출력하고 싶을 때는 아래 스크립트를 이용한다.
