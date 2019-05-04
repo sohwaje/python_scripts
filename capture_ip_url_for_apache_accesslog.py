3761145e6ce9c75e4c94b16f82feb40163f2e335
sohwaje <kissenyo@gmail.com>
2019-05-04 16:12:09 +0900

    dd

--- capture_ip_url_for_apache_accesslog.py
+++ capture_ip_url_for_apache_accesslog.py
@@ -1,13 +1,11 @@
 # 아파치 Access_log에서 IP와 URL을 추출한다.
-"""사용법
-]# python3.7 accesslog.py 아파치엑세스로그파일명
-"""
-"""
-정규식 문자 패턴 분석 : https://regex101.com/r/lIHC9G/1
+"""usage
+]> python3.7 script_name apache_access.log
+regular expressions : https://regex101.com/r/lIHC9G/1
 """
 from sys import argv
 import re
-script, file_name = argv                        # 스크립트를 실행 할 때 입력 파일(여기서는 아파치 엑세스 로그)을 1개 받는다.
+script, file_name = argv
 
 file = open(file_name, 'r')
 lines = file.readlines()
