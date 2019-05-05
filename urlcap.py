caa36c4c8fb299f7d368227a66654f18c0d4b196
sohwaje <kissenyo@gmail.com>
2019-05-05 16:06:50 +0900

    apache_log_cap.py

--- urlcap.py
+++ urlcap.py
@@ -1,19 +1,18 @@
 import requests
 import re
-
-# url의 응답코드가 200ok이면, url을 html이라는 변수에 text로 저장하는 함수
+# if 200 ok -> catch a url and save
 def get(url):
     resp = requests.get(url)
     if resp.status_code == requests.codes.ok:
         html = resp.text
     return html
 
-# 패턴 함수
+# pattern def
 def pattern(p):
     result = re.compile(p)
     return result
 
-# URL 입력을 받음
+# input a URL
 #URL = 'http://www.naver.com'
 URL = input('URL> ')
 html = get(URL)
@@ -24,9 +23,9 @@ text = pattern(p)
 # html에서 패턴과 일치하는 항목을 찾아 list 형식으로 result에 저장.
 result = text.findall(html)
 
-# url의 길이값(length) 순서로 정렬하여 저장
+# sorted key len
 sort = sorted(result, key=len)
 
-#sort 리스트에서 url을 출력한다.
+#print url in list
 for url in sort:
     print(url)
