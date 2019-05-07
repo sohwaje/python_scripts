b08f971eb5b71f54b060d477b9ae5bd57b252d14
sohwaje <kissenyo@gmail.com>
2019-05-07 22:08:27 +0900

    Full url, Short url, url and response time check

--- urlcap.py
+++ urlcap.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3.7
 import requests
 import re
+import datetime
+import time
+import ssl
 
-# 사이트 유효성 검사 및 사이트 내용 저장 함수
+# 사이트 유효성 검사 및 사이트 내용 저장
 def get(url):
     resp = requests.get(url)
     if resp.status_code == requests.codes.ok:       # if 200 ok -> catch a url and save function
         html = resp.text
+    else:
+        print('%s is Check : Response Status :%d' %(url, resp.status_code))
+        exit(0)
     return html
 
-# pattern 컴파일 함수
+# pattern 정규식 컴파일 함수
 def pattern(x):
     result = re.compile(x)
     return result
@@ -20,9 +26,9 @@ def full_url():
     html = get(URL)
     p = 'http[s]?://(?:[a-zA-Z]|[0-9][-]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'    # FULL URL 캡쳐 정규식
     text = pattern(p)
-    result = text.findall(html)                      # html에서 fullurl을 추출하여 result에 담는다.
-    sort = sorted(result, key=len)                   # URL 길이에 따라 정렬
-    for url in sort:                                 # URL 리스트에서 URL 출력
+    result = text.findall(html)
+    sort = sorted(result, key=len)
+    for url in sort:
         print(url)
 
 # only www function (WWW URL 출력)
@@ -36,6 +42,17 @@ def only_www():
     for www in sort:
         print(www)
 
+def respones_time():
+    URL = input('URL> ')
+    html = get(URL)
+    p = 'http[s]?://(?:[a-zA-Z]|[0-9][-]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'    # FULL URL 캡쳐 정규식
+    text = pattern(p)
+    result = text.findall(html)
+    sort = sorted(result, key=len)
+    for respon in result:
+        rp = requests.get(respon)
+        print(rp.elapsed, respon)
+
 # exit function
 def End_of_script(x):
     print(x, "Good Bye")
@@ -43,8 +60,9 @@ def End_of_script(x):
 
 # start function
 def start():
-    print("Capture full url select: 1, ex)http://www.abc.com/abc/&879/778_ga.gif")
-    print("Capture www select: 2, ex)http://www.abc.com")
+    print("Capture full url     : 1,  ex)http://www.abc.com/abc/&879/778_ga.gif")
+    print("Capture short url    : 2,  ex)http://www.abc.com")
+    print("Capture url and response time : 3 ")
 
     choice = input("> ")
 
@@ -52,6 +70,8 @@ def start():
         full_url()
     elif choice == "2":
         only_www()
+    elif choice =="3":
+        respones_time()
     else:
         End_of_script("Thank you")
 
