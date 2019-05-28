3e0fdeaf89296082203eaabeb6dc65817f7229f5
sohwaje <kissenyo@gmail.com>
2019-05-28 16:41:10 +0900

    Update sysmon_alarm.py

--- sysmon_alarm.py
+++ sysmon_alarm.py
@@ -23,8 +23,8 @@ title = '[' + serverName + ' 서버]\n'
 msg = title
 
 #Telegram bot 설정
-sigong_token = '851723999:AAFUkV3XFAHbujWNbbJO2AXr6dr3SKg8AWA'
-sigong = '-1001374734756'
+sigong_token = 'YOUR_TOKEN'
+sigong = 'Chat_ID'
 bot = telegram.Bot(token = sigong_token)
 
 """
