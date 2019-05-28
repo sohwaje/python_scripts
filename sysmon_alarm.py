236c5dd60103d7209f88146d405c7c59b310de69
sohwaje <kissenyo@gmail.com>
2019-05-28 15:50:59 +0900

    Update sysmon_alarm.py

--- sysmon_alarm.py
+++ sysmon_alarm.py
@@ -11,18 +11,20 @@ import psutil
 """
 임계치 설정
 """
-cpuLimit = 0
-memLimit = 10
-loadAverage = 0
-swapUsage = 2
+cpuLimit = 20
+memLimit = 90
+loadAverage = 3.0
+swapUsage = 10
+root_disk = 90
+home_disk = 90
 #==============================================================================
-serverName = 'was1'
+serverName = 'sigong_messenger_was1(26.145)'
 title = '[' + serverName + ' 서버]\n'
 msg = title
 
 #Telegram bot 설정
-sigong_token = 'YOUR_TOKEN'
-sigong = YOUR_ID
+sigong_token = '851723999:AAFUkV3XFAHbujWNbbJO2AXr6dr3SKg8AWA'
+sigong = '-1001374734756'
 bot = telegram.Bot(token = sigong_token)
 
 """
@@ -34,7 +36,7 @@ def send(chat):
 LoadAverage
 """
 def getLoadAverage():
-    return round(psutil.getloadavg()[0])
+    return psutil.getloadavg()[0]
 
 """
 cpu 사용률
@@ -56,10 +58,27 @@ Memory 사용량
 def getMemUsage():
     return round(psutil.virtual_memory()[2])
 
+"""
+루트 디스크(/) 사용량
+"""
+def getRootdisk():
+    return psutil.disk_usage('/')[3]
+
+"""
+/home 디스크 사용량
+"""
+def getHomedisk():
+    return round(psutil.disk_usage('/home')[3])
+
+"""
+현재 사용량
+"""
 loadAvr = getLoadAverage()
 avgCpu = getCpuUsage()
 memUsage = getMemUsage()
 swap = getSwapUsage()
+root_Disk = getRootdisk()
+home_Disk = getHomedisk()
 
 if loadAvr > loadAverage:
         msg += 'LoadAverage(' + str(loadAverage) + ') 초과\n'
@@ -79,4 +98,14 @@ if memUsage > memLimit:
 if swap > swapUsage:
         msg += 'swap 메모리 사용량 기준값(' + str(swapUsage) + '%) 초과\n'
         msg += 'swap 메모리 사용량 : ' + str(swap) + '%\n'
+        send(msg)
+
+if root_Disk > root_disk:
+        msg += '디스크(/) 사용량 기준값(' + str(root_disk) + '%) 초과\n'
+        msg += '현재 루트 디스크 사용량 : ' + str(root_Disk) + '%\n'
+        send(msg)
+
+if home_Disk > home_disk:
+        msg += '디스크(/home) 사용량 기준값(' + str(home_disk) + '%) 초과\n'
+        msg += '현재 (/home) 디스크 사용량 : ' + str(home_Disk) + '%\n'
         send(msg)
