31161af9c1d306c25ec5ed6146bd230e4440d178
sohwaje <kissenyo@gmail.com>
2019-06-13 21:51:02 +0900

    update

--- sysmon_alarm_daemon.py
+++ sysmon_alarm_daemon.py
@@ -11,8 +11,8 @@ title = '[' + serverName + ' 서버]\n'
 """
 [2]Telegram bot token value
 """
-sigong_token = '851723999:AAFUkV3XFAHbujWNbbJO2AXr6dr3SKg8AWA'
-sigong = '137532606'
+sigong_token = 'YOUR_TOKEN'
+sigong = 'YOUR_CHAT_ID'
 bot = telegram.Bot(token = sigong_token)
 
 """
