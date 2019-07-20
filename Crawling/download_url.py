05820108ebca0e41e430f16e054cd35c797b452e
sohwaje <kissenyo@gmail.com>
2019-07-20 16:09:36 +0900

    20180720

--- Crawling/download_url.py
+++ Crawling/download_url.py
@@ -3,7 +3,7 @@ from urllib.error import URLError, HTTPError, ContentTooShortError
 """
 1. 웹페이지 다운로드
 2. 다운로드 재처리
-3. 사용자 에이전트 설정
+3. 사용자 에이전트 설정 : 기본 사용자 에이전트를 wswp(Web Scraping with Python라는 의미)로 설정한다.
 """
 """
 hasattr 함수: (e, 'code') 에러에 'code' 변수가 있는지 확인한다.
@@ -12,10 +12,10 @@ wswp(Web Scraping with Python)
 def download(url, retries=2, user_agent='wswp', charset='utf-8'):
     print('다운로드:', url)
     request = urllib.request.Request(url)
-    request.add_header('User-agent', user_agent)
+    request.add_header('User-agent', user_agent) # 사용자 에이전트를 추가한다.
     try:
-        html = urllib.request.urlopen(url).read()
-    except (URLError, HTTPError, ContentTooShortError) as e:
+        html = urllib.request.urlopen(request).read() # urlopen 함수는 웹 페이지를 다운로드 하고 html을 리턴한다.
+    except (URLError, HTTPError, ContentTooShortError) as e: # 다운로드 또는 url 에러에 대한 예외 처리.
         print('다운로드 에러:', e.reason)
         html = "알 수 없는 오류가 발생하였습니다."
         if retries > 0:
@@ -27,3 +27,13 @@ def download(url, retries=2, user_agent='wswp', charset='utf-8'):
 
 var = "http://httpstat.us/500"
 print(download(var))
+
+"""결과
+다운로드: http://httpstat.us/500
+다운로드 에러: Internal Server Error
+다운로드: http://httpstat.us/500
+다운로드 에러: Internal Server Error
+다운로드: http://httpstat.us/500
+다운로드 에러: Internal Server Error
+알 수 없는 오류가 발생하였습니다.
+"""
---
+++ Crawling/sitemap_crawling.py
@@ -0,0 +1,46 @@
+import urllib.request
+from urllib.error import URLError, HTTPError, ContentTooShortError
+import re
+
+def download(url, retries=2, user_agent='wswp', charset='utf-8'):
+    print('다운로드:', url)
+    request = urllib.request.Request(url)
+    request.add_header('User-agent', user_agent)
+    try:
+        resp = urllib.request.urlopen(request)
+        cs = resp.headers.get_content_charset() # headers 철자 주의.
+        if not cs:
+            cs = charset
+        html = resp.read().decode(cs)
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print('다운로드 에러:', e.reason)
+        html = "알 수 없는 오류가 발생하였습니다." # 또는 html = None
+        if retries > 0:
+            if hasattr(e, 'code') and 400 <= e.code < 600: #4xx, 5xx 에러가 발생하면 재귀적 재시도
+                return download(url, retries - 1)
+    return html
+
+def crawl_sitemap(url):
+    # 사이트 맵 파일을 다운로드 한다.
+    sitemap = download(url)
+    # 사이트 맵 링크를 추출한다.
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    # 각 링크를 다운로드 한다.
+    for link in links:
+        html = download(link)
+
+var = "http://example.webscraping.com/sitemap.xml"
+print(crawl_sitemap(var))
+
+"""출력 결과
+다운로드: http://example.webscraping.com/sitemap.xml
+다운로드: http://example.webscraping.com/places/default/view/Afghanistan-1
+다운로드: http://example.webscraping.com/places/default/view/Aland-Islands-2
+다운로드: http://example.webscraping.com/places/default/view/Albania-3
+다운로드: http://example.webscraping.com/places/default/view/Algeria-4
+다운로드: http://example.webscraping.com/places/default/view/American-Samoa-5
+다운로드: http://example.webscraping.com/places/default/view/Andorra-6
+다운로드: http://example.webscraping.com/places/default/view/Angola-7
+다운로드: http://example.webscraping.com/places/default/view/Anguilla-8
+다운로드: http://example.webscraping.com/places/default/view/Antarctica-9
+"""
