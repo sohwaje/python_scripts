ae02428fa2bde15f9c07d6aa1a36e4d334127309
sohwaje <kissenyo@gmail.com>
2020-05-13 15:42:44 +0900

    포트 스캐너

--- local_port_scanner.py
+++ local_port_scanner.py
@@ -1,15 +1,23 @@
-# local_port_scanner.py
-import socket
-
-def find_service_name():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    protocolname = 'tcp'
-    for port in range(1, 1024):
-        result = sock.connect_ex(('127.0.0.1',port))
-        if result == 0:
-            print(port, 'port is open')
-        else:
-            print(port, 'port is not open')
-
+# -*- coding: utf-8 -*-
+import socket, sys
+from sys import argv
+script, host = argv
+"""
+python2.x, python3.x
+사용법: # python port_scanner.py 127.0.0.1
+"""
+def check_port():
+    port_open=[]
+    for port in range(1, 65536):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(5);
+            result = s.connect_ex((host,port))
+            if result == 0:
+                port_open.append(port)
+                print(port, 'port is open')
+        except (socket.gaierror, socket.error, socket.timeout) as e:
+            pass
+    print("열린 포트 : %s" % (port_open))
 if __name__ == '__main__':
-    find_service_name()
+    check_port()
