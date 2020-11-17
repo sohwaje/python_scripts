806719616471e24bfba28035a18c40e49e917a03
kissenyo <sohwaje@gmail.com>
2020-11-18 08:59:30 +0900

    ADD create a new container

--- azure_for_python/create_container_blob.py
+++ azure_for_python/create_container_blob.py
@@ -8,10 +8,11 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, _
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 local_path = "/home/azureuser"
 local_file_name = "ls" + ".sh"
+upload_container_name = "samplecontainer"
 
 def Create_container_and_upload_file():
     upload_file_path = os.path.join(local_path, local_file_name)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+    blob_client = blob_service_client.get_blob_client(container=upload_container_name, blob=local_file_name)
     print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
     with open(upload_file_path, "rb") as data:
         blob_client.upload_blob(data)
@@ -21,20 +22,19 @@ try:
     # Create the BlobServiceClient object which will be used to create a container client
     # 컨테이너 클라이언트를 만드는 데 사용할 blob서비스 클라이언트 오브젝트를 생성한다.
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    container_name = "samplecontainer"
     container_list=[]
     all_containers = blob_service_client.list_containers()
     for container in all_containers:
         container_list.append(container['name'])
-    if container_name not in container_list:
-        print("container_name : " + container_name + " dose not exist.")
-        container_client = blob_service_client.create_container(container_name)
+    if upload_container_name not in container_list:
+        print("upload_container_name : " + upload_container_name + " dose not exist.")
+        container_client = blob_service_client.create_container(upload_container_name)
         Create_container_and_upload_file()
     else:
-        print("container_name : " + container_name + " dose exist.")
-        container_name = "sample" + str(uuid.uuid4())
-        print("new container_name : " + container_name)
-        container_client = blob_service_client.create_container(container_name)
+        print("upload_container_name : " + upload_container_name + " dose exist.")
+        upload_container_name = "sample" + str(uuid.uuid4())    # 새로운 컨테이너 생성
+        print("new upload_container_name : " + upload_container_name)
+        container_client = blob_service_client.create_container(upload_container_name)
         Create_container_and_upload_file()
 except Exception as ex:
     print(ex)
