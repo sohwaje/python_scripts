88503188618faee4b0664e46e583a4d04fb92ae3
sohwaje <kissenyo@gmail.com>
2021-05-13 21:13:41 +0900

    주석 추가

--- azure_for_python/create_container_upload_blob.py
+++ azure_for_python/create_container_upload_blob.py
@@ -47,20 +47,20 @@ def upload_file():
 try:
     print("Azure Blob storage v" + __version__ + " - Python quickstart")
     # 컨테이너 클라이언트를 만드는 데 사용할 blob서비스 클라이언트 오브젝트를 생성한다.
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)     # 연결 문자열을 통해 blob서비스에 인증한다.
     container_list=[]
     all_containers = blob_service_client.list_containers()          # 모든 컨테이너 리스트를 구함.
     for container in all_containers:                                # 모든 컨테이너에서 생성할 컨테이너를 찾음
         container_list.append(container['name'])                    # 컨테이너 리스트에 해당 컨테이너를 추가
-    if upload_container_name not in container_list:
+    if upload_container_name not in container_list:                 # container_list 안에 upload_container_name이 없으면 True
         print("upload_container_name : " + upload_container_name + " dose not exist.")
         container_client = blob_service_client.create_container(upload_container_name)
         upload_file()
     else:
         print("upload_container_name : " + upload_container_name + " dose exist.")
-        upload_container_name = "sample" + str(uuid.uuid4())    # 새로운 컨테이너 생성
-        print("new upload_container_name : " + upload_container_name)
-        container_client = blob_service_client.create_container(upload_container_name)
+        upload_container_name = "sample" + str(uuid.uuid4())                # hiclass 컨테이너가 있으면 새 컨테이너가 필요하며 새 컨테이너 이름은 sample로 시작한다.
+        print("new upload_container_name : " + upload_container_name)       # 생성될 컨테이너 이름 출력
+        container_client = blob_service_client.create_container(upload_container_name) # sample 컨테이너를 생성한다
         upload_file()
         # print("upload_container_name : " + upload_container_name + " dose not exist. Create container first.") 새 컨테이너가 필요 없을 때 출력
 except Exception as ex:
