3423f3e3f6a74049f0803b45e7fc7aafb6067c3b
sohwaje <kissenyo@gmail.com>
2021-05-17 09:46:28 +0900

    ㅇㄹㄴㅇㄹㄴㅇㄹ

--- azure_for_python/parallel_download_all_blobs_in_container.py
+++ azure_for_python/parallel_download_all_blobs_in_container.py
@@ -17,7 +17,8 @@ LOCAL_BLOB_PATH = ""
 
 
 # Azure 스토리지 계정에 접근하기 위한 인스턴스
-blob_service_client =  BlobServiceClient.from_connection_string(CONNECTION_STRING)
+blob_service_client = BlobServiceClient.from_connection_string(
+    CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(BLOB_CONTAINER)
 
 
@@ -26,19 +27,25 @@ def download_all_blobs_in_container():
     result = run(my_blobs)
     print(result)
 
+
 def run(blobs):
     with ThreadPool(processes=int(10)) as pool:
         return pool.map(save_blob_locally, blobs)
 
+
 def save_blob_locally(blob):
     file_name = blob.name
     print(file_name)
-    bytes = container_client.get_blob_client(blob).download_blob().readall() # blob을 읽어서 bytes 인스턴스에 넣는다.
-    download_file_path = os.path.join(LOCAL_BLOB_PATH, file_name)   # /LOCAL_BLOB_PATH/file_name
-    os.makedirs(os.path.dirname(download_file_path), exist_ok=True) # 하위 디렉토리까지 생성
+    # blob을 읽어서 bytes 인스턴스에 넣는다.
+    bytes = container_client.get_blob_client(blob).download_blob().readall()
+    # 다운로드 경로 : /LOCAL_BLOB_PATH/file_name
+    download_file_path = os.path.join(LOCAL_BLOB_PATH, file_name)
+    os.makedirs(os.path.dirname(download_file_path),
+                exist_ok=True)  # 하위 디렉토리까지 생성
 
     with open(download_file_path, "wb") as file:
-      file.write(bytes) # bytes 인스턴스의 블롭을 쓴다
+        file.write(bytes)  # bytes 인스턴스의 블롭을 쓴다
     return file_name
+
 
 download_all_blobs_in_container()
