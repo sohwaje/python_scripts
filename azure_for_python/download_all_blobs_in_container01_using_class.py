6f9844057e125cbc2862ea0887dc0383bfd70c60
sohwaje <kissenyo@gmail.com>
2021-05-17 14:21:32 +0900

    update python azure blob downloader

---
+++ azure_for_python/download_all_blobs_in_container01_using_class.py
@@ -0,0 +1,53 @@
+# https://www.quickprogrammingtips.com/azure/how-to-download-blobs-from-azure-storage-using-python.html
+# Python program to bulk download blob files from azure storage
+# Uses latest python SDK() for Azure blob storage
+# Requires python 3.6 or above
+# pip3 install azure-storage-blob --user
+# -*- coding: utf-8 -*-
+import os
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+
+# azure blob 스토리지 연결
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+# 로컬 파일 경로
+LOCAL_BLOB_PATH = "D:\TEST_DOWNLOAD"
+# 업로드 경로: azure blob container
+BLOB_CONTAINER = "testapibuild"
+
+
+class AzureBlobFileDownloader:
+    def __init__(self):
+        # blob 스토리지 연결 문자열을 통해 blob에 접근하는 인스턴스 생성
+        self.blob_service_client = BlobServiceClient.from_connection_string(
+            connect_str)
+        self.container_client = self.blob_service_client.get_container_client(
+            BLOB_CONTAINER)
+
+    def save_blob(self, file_name, file_content):
+        download_file_path = os.path.join(
+            LOCAL_BLOB_PATH, file_name)   # /LOCAL_BLOB_PATH/file_name
+        # for nested blobs, create local path as well!
+        os.makedirs(os.path.dirname(download_file_path),
+                    exist_ok=True)  # 하위 디렉토리까지 생성
+        # download_file_path를 쓰기 모드로 열고 file이라는 별칭으로 치환한다.
+        with open(download_file_path, "wb") as file:
+            # download_file_path 요소들에 file_content를 쓴다
+            file.write(file_content)
+        """
+        ex) with open("foo.txt", "w") as f:
+                f.write("Life is too short, you need python")
+        """
+
+    def download_all_blobs_in_container(self):
+        # blob 리스트가 있는 my_blobs 인스턴스 생성
+        my_blobs = self.container_client.list_blobs()
+        print("my_blobs:", my_blobs)
+        for blob in my_blobs:                               # my_blobs 인스턴스에서 blob을 하나씩 꺼냄
+            print(blob.name)
+            # blob의 콘텐츠를 하나하나 읽으면서 다운로드한다.
+            bytes = self.container_client.get_blob_client(blob).download_blob().readall()
+            self.save_blob(blob.name, bytes)
+
+
+go_download = AzureBlobFileDownloader()
+go_download.download_all_blobs_in_container()
--- azure_for_python/download_all_blobs_in_container02.py
+++ azure_for_python/download_all_blobs_in_container02.py
@@ -1,5 +1,9 @@
-#https://www.quickprogrammingtips.com/azure/how-to-download-blobs-from-azure-storage-using-python.html
-import re, sys, os, uuid, glob
+# https://www.quickprogrammingtips.com/azure/how-to-download-blobs-from-azure-storage-using-python.html
+import re
+import sys
+import os
+import uuid
+import glob
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
 
 """
@@ -8,7 +12,7 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, _
 # azure blob 스토리지 연결
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 # 로컬 파일 경로
-LOCAL_BLOB_PATH = "/Users/yusunglee/Downloads/slack"
+LOCAL_BLOB_PATH = "D:\TEST_DOWNLOAD"
 # 업로드 경로: azure blob container
 BLOB_CONTAINER = "testapibuild"
 
@@ -18,29 +22,38 @@ container_client = blob_service_client.get_container_client(BLOB_CONTAINER)
 """
 blob 리스트 구하는 함수
 """
+
+
 def get_list_all_blob_in_container():
     blob_list = container_client.list_blobs()
     return blob_list
 
+
 """
 저장 함수
 """
-def save_blob(file_name,file_content):
+
+
+def save_blob(file_name, file_content):
     download_file_path = os.path.join(LOCAL_BLOB_PATH, file_name)
     os.makedirs(os.path.dirname(download_file_path), exist_ok=True)
     with open(download_file_path, "wb") as download_file:
         download_file.write(file_content)
 
+
 """
 다운로드 함수
 """
+
+
 def download_all_blobs_in_container():
     my_blobs = get_list_all_blob_in_container()
     print("my_blobs: ", my_blobs)
     for blob in my_blobs:
         print(blob.name)
-        bytes = container_client.get_blob_client(blob).download_blob().readall()
-        save_blob(blob.name, bytes) # blob.name = blob 이름, bytes = blob 내용
+        bytes = container_client.get_blob_client(
+            blob).download_blob().readall()
+        save_blob(blob.name, bytes)
 
 
 download_all_blobs_in_container()
